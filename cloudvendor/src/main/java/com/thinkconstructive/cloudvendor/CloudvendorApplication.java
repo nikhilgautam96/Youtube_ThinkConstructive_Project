80d9c4a04c0b1247be3121e4cc989c51a237ea9a
nikhilgautam96 <nikhilgautam1729@gmail.com>
2024-09-29 18:50:47 +0530

    starting the project | Boilerplate code

---
+++ cloudvendor/src/main/java/com/thinkconstructive/cloudvendor/CloudvendorApplication.java
@@ -0,0 +1,13 @@
+package com.thinkconstructive.cloudvendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudvendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudvendorApplication.class, args);
+	}
+
+}
---
+++ cloudvendor/src/test/java/com/thinkconstructive/cloudvendor/CloudvendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkconstructive.cloudvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudvendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
